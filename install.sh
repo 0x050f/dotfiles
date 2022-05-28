29eac5cbb94383e89fcd0116f6bde449ad9ea35c
Yellow <user@localhost.localdomain>
2022-05-28 17:39:30 +0200

    adding 42 header

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 #    By: lmartin <lmartin@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/04 04:43:15 by lmartin           #+#    #+#              #
-#    Updated: 2020/02/04 04:43:18 by lmartin          ###   ########.fr        #
+#    Updated: 2020/06/16 18:49:15 by ska              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
@@ -15,6 +15,9 @@
 vimrc="vimrc"
 plugins="plugins"
 theme="theme"
+
+user="lmartin"
+mail=$user@student.42.fr
 
 # unknow command
 if [[ -n $1 ]] && [[ $1 != $vimrc ]] && [[ $1 != $plugins ]] && [[ $1 != $theme ]]
@@ -33,8 +36,16 @@ fi
 if [[ $1 = $plugins ]] || [[ -z $1 ]]
 then
 	## delete if exist
+	rm -rf $HOME/.vim/plugins
 	rm -rf $HOME/.vim/pack/plugins/start/vim-airline
 	rm -rf $HOME/.vim/pack/plugins/start/lightline
+	# header 42
+	mkdir -p $HOME/.vim/plugins
+	cp -f ./stdheader.vim $HOME/.vim/plugins/stdheader.vim
+	if [[ -z "${USER}" ]]; then
+		echo "export USER=$user" >> $HOME/.zshrc
+	if [[ -z "${MAIL}" ]]; then
+		echo "export MAIL=$mail" >> $HOME/.zshrc
 	## airline
 	mkdir -p $HOME/.vim/pack/plugins/start/vim-airline
 	git clone https://github.com/vim-airline/vim-airline.git $HOME/.vim/pack/plugins/start/vim-airline
