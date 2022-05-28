46065fa6573b74bb0a3f9a63058449d20325ab9b
Yellow <user@localhost.localdomain>
2022-05-28 17:39:30 +0200

    plugin not plugins

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 #    By: lmartin <lmartin@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/04 04:43:15 by lmartin           #+#    #+#              #
-#    Updated: 2020/06/20 05:48:53 by lmartin          ###   ########.fr        #
+#    Updated: 2020/06/21 09:10:30 by lmartin          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
@@ -35,11 +35,11 @@ fi
 if [[ $1 = $plugins ]] || [[ -z $1 ]]
 then
 	## delete if exist
-	rm -rf $HOME/.vim/plugins
+	rm -rf $HOME/.vim/plugin/stdheader.vim
 	rm -rf $HOME/.vim/pack/plugins/start/vim-airline
 	rm -rf $HOME/.vim/pack/plugins/start/lightline
 	# header 42
-	mkdir -p $HOME/.vim/plugins
+	mkdir -p $HOME/.vim/plugin
 	cp -f ./stdheader.vim $HOME/.vim/plugins/stdheader.vim
 	if [[ -z "${USER}" || $USER != $user ]]
 	then
