39f97340ed9058122cf912d43235b797a9a203f3
Yellow <user@localhost.localdomain>
2022-05-28 17:39:30 +0200

    comment things

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 #    By: lmartin <lmartin@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/04 04:43:15 by lmartin           #+#    #+#              #
-#    Updated: 2021/03/02 12:46:04 by lmartin          ###   ########.fr        #
+#    Updated: 2021/05/20 10:15:26 by lmartin          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
@@ -21,28 +21,29 @@ cpy_vimrc() {
 
 setup_plugins() {
 	rm -rf $HOME/.vim/plugin
-	rm -rf $HOME/.vim/pack/plugins/start/lightline
 	mkdir -p $HOME/.vim/plugin
 
-	curl https://www.vim.org/scripts/download_script.php?src_id=14064 > DoxygenToolkit.vim
-	cp -f ./DoxygenToolkit.vim $HOME/.vim/plugin/DoxygenToolkit.vim
+##	Doxygen
+#	curl https://www.vim.org/scripts/download_script.php?src_id=14064 > DoxygenToolkit.vim
+#	cp -f ./DoxygenToolkit.vim $HOME/.vim/plugin/DoxygenToolkit.vim
+#
+##	42 header
+#	if [ ! -f /usr/share/vim/vim80/plugin/stdheader.vim ] ; then
+#	cp -f ./stdheader.vim $HOME/.vim/plugin/stdheader.vim
+#	if [ -z "${FT_USER}" ] || ! [ $FT_USER = $user ] ; then
+#		if ! grep "export FT_USER=$user" $HOME/.zshrc ; then
+#			echo "export FT_USER=$user" >> $HOME/.zshrc
+#		fi
+#	fi
+#	if [ -z "${MAIL}" ] || ! [ $MAIL = $mail ] ; then
+#		if ! grep "export MAIL=$mail" $HOME/.zshrc ; then
+#			echo "export MAIL=$mail" >> $HOME/.zshrc
+#		fi
+#	fi
+#	fi
 
-	if [ ! -f /usr/share/vim/vim80/plugin/stdheader.vim ] ; then
-	cp -f ./stdheader.vim $HOME/.vim/plugin/stdheader.vim
-	if [ -z "${FT_USER}" ] || ! [ $FT_USER = $user ] ; then
-		if ! grep "export FT_USER=$user" $HOME/.zshrc ; then
-			echo "export FT_USER=$user" >> $HOME/.zshrc
-		fi
-	fi
-	if [ -z "${MAIL}" ] || ! [ $MAIL = $mail ] ; then
-		if ! grep "export MAIL=$mail" $HOME/.zshrc ; then
-			echo "export MAIL=$mail" >> $HOME/.zshrc
-		fi
-	fi
-	fi
-
-	mkdir -p $HOME/.vim/pack/plugins/start/lightline
-	git clone https://github.com/itchyny/lightline.vim $HOME/.vim/pack/plugins/start/lightline
+#	mkdir -p $HOME/.vim/pack/plugins/start/lightline
+#	git clone https://github.com/itchyny/lightline.vim $HOME/.vim/pack/plugins/start/lightline
 }
 
 setup_theme() {
@@ -60,8 +61,8 @@ setup_theme() {
 	mkdir -p $HOME/.vim/doc/
 	cp -f ./edge/doc/edge.txt $HOME/.vim/doc/
 
-	mkdir -p $HOME/.vim/autoload/lightline/colorscheme/
-	cp -f ./edge/autoload/lightline/colorscheme/* $HOME/.vim/autoload/lightline/colorscheme/
+#	mkdir -p $HOME/.vim/autoload/lightline/colorscheme/
+#	cp -f ./edge/autoload/lightline/colorscheme/* $HOME/.vim/autoload/lightline/colorscheme/
 
 	rm -rf ./edge
 }
@@ -70,8 +71,9 @@ setup_languages() {
 	rm -rf $HOME/.vim/syntax
 	mkdir -p $HOME/.vim/syntax
 
-	git clone https://github.com/dart-lang/dart-vim-plugin
-	cp -f dart-vim-plugin/syntax/dart.vim $HOME/.vim/syntax/
+##	Clone and add new languages syntax here
+#	git clone https://github.com/dart-lang/dart-vim-plugin
+#	cp -f dart-vim-plugin/syntax/dart.vim $HOME/.vim/syntax/
 }
 
 main() {
